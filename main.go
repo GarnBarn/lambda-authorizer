ff691ca3859968636c948cd917a84eeadd49041e
Siratee K <sirateek@gmail.com>
2023-04-21 21:39:20 +0700

    Update to check the useruid key before letting them pass into the system

--- main.go
+++ main.go
@@ -14,15 +14,18 @@ import (
 
 func HandleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequestTypeRequest) (events.APIGatewayV2CustomAuthorizerSimpleResponse, error) {
 
+	authResponse := events.APIGatewayV2CustomAuthorizerSimpleResponse{
+		IsAuthorized: false,
+	}
+
+	if _, ok := event.Headers["x-gb-user"]; ok {
+		return authResponse, errors.New("Unauthorized")
+	}
 	// Initilize the Firebase App
 	opt := option.WithCredentialsFile("firebase-credential.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		logrus.Fatalln("error initializing app: %v\n", err)
-	}
-
-	authResponse := events.APIGatewayV2CustomAuthorizerSimpleResponse{
-		IsAuthorized: false,
 	}
 
 	authClient, err := app.Auth(ctx)
